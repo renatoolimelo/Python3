56669c93fef66ff9a4a652f4b58a43c76939bb9a
Renato Oliveira de Melo <renato.olimelo@gmail.com>
2020-02-25 21:13:36 -0300

    Fim da aula 5.4

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -6,15 +6,20 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while (rodada <= total_de_tentativas ):
+##while (rodada <= total_de_tentativas ):
+for rodada in range (1,total_de_tentativas + 1):
 
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
-    chute_str = input("Digite o seu numero: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
 
     print("Você digitou ", chute_str)
 
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um núemro entre 1 e 100!")
+        continue
 
     # if (numero_secreto == chute):
     #    print("Você acertou")
@@ -27,19 +32,18 @@ while (rodada <= total_de_tentativas ):
 
     if (acertou):
         print("Você acertou")
+        break
     elif (maior):
         print("Você errou! O seu chute foi maior do que o número secreto")
     elif (menor):
         print("Você errou! O seu chute foi menor do que o número secreto")
 
-    rodada = rodada + 1
-
 print("Fim do jogo")
 
 ###########################################################
 
-numero1 = 10
-numero2 = "20"
-produto = numero1 * numero2
-print(produto)
+#numero1 = 10
+#numero2 = "20"
+#produto = numero1 * numero2
+#print(produto)
 ##resultado 20202020202020202020
