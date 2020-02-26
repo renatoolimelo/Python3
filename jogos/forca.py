42873efee9f40a9e3555e2de71a1a3863638df9e
Renato Oliveira de Melo <renato.olimelo@gmail.com>
2020-02-26 12:54:43 -0300

    Aula2.2.5

--- jogos/forca.py
+++ jogos/forca.py
@@ -5,11 +5,27 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    ##palavra_secreta.capitalize() banana == Banana
+    ##palavra_secreta.endswith("na") banana == true  palavra_secreta.endswith("naa") banana == false
 
     enforcou = False
     acertou  = False
 
+
     while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip(); #remover espaços.
+        index = 0
+
+        for letra in palavra_secreta:
+            #if (chute.lower() == letra):
+                #print(chute.lower())
+
+            index = index + 1
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+
         print("Jogando ...")
 
     print("Fim do jogo")
