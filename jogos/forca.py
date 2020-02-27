07e85659bda2fdcfe55db1b27571b57600c19e5b
Renato Oliveira de Melo <renato.olimelo@gmail.com>
2020-02-26 21:00:17 -0300

    Aula 2.7.3

--- jogos/forca.py
+++ jogos/forca.py
@@ -5,56 +5,27 @@ def jogar():
     imprime_mensagem_abertura()
     palavra_secreta = carrega_palavra_secreta()
     letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
 
     enforcou = False
     acertou  = False
     tentativas = 6
 
-    print(letras_acertadas)
-
     while(not enforcou and not acertou):
 
-        chute = input("Qual letra? ")
-        chute = chute.strip().upper()     #remover espaços, e deixar tudo maiusculo
-
+        chute = pede_chute()
         if (chute in palavra_secreta):
-
-            index = 0
-
-            for letra in palavra_secreta:
-                #if (chute.lower() == letra):
-                    #print(chute.lower())
-
-                if (chute == letra):
-                    #print("Encontrei a letra {} na posição {}".format(letra, index + 1))
-                    letras_acertadas[index] = letra
-
-                index += 1
-
-            print()
-            print("Você acertou!")
-
+            marca_chute_correto(palavra_secreta, chute, letras_acertadas)
         else:
-
             tentativas -= 1
             print()
             print("Ops, você errou! Restam {} tentativas!".format(tentativas))
-
         if("_" in letras_acertadas):
-
-            letras_faltando = str(letras_acertadas.count('_'))
-            print()
-            print("Ainda faltam acertas {} letras".format(letras_faltando))
-            print(letras_acertadas)
-            print()
-
+            letras_faltando(letras_acertadas)
         else:
-            print(letras_acertadas)
-            print("Você ganhou!")
+            venceu_jogo(letras_acertadas)
             break
-
         enforcou = tentativas == 0
-
     if(enforcou):
         print("Você perdeu!")
 
@@ -77,6 +48,30 @@ def carrega_palavra_secreta():
 
 def inicializa_letras_acertadas(palavra_secreta):
     return ["_" for letra in palavra_secreta]
+
+def pede_chute():
+    chute = input("Qual letra? ")
+    return chute.strip().upper()
+
+def marca_chute_correto(palavra_secreta, chute, letras_acertadas):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute == letra):
+            letras_acertadas[index] = letra
+        index += 1
+    print()
+    print("Você acertou!")
+
+def letras_faltando(letras_acertadas):
+    letras_faltando = str(letras_acertadas.count('_'))
+    print()
+    print("Ainda faltam acertas {} letras".format(letras_faltando))
+    print(letras_acertadas)
+    print()
+
+def venceu_jogo(letras_acertadas):
+    print(letras_acertadas)
+    print("Você ganhou!")
 
 if(__name__ == "__main__"):
     jogar()
