7deeed943b027a212f3445ce03facf62b191ec93
“Renato” <renato.olimelo@gmail.com>
2020-06-02 19:41:51 -0300

    aula de numeros e strings Udemy

---
+++ Curso_udemy/numeros.py
@@ -0,0 +1,32 @@
+print("Hello World")
+
+#int
+print(type(1))
+
+#float
+print(type(1.1))
+
+#calculo
+print(1+1)
+
+#expoente 2 elevado a 3
+print(2**3)
+
+#raiz cubica de 8
+print(8**(1/3))
+
+#atribuir variavel
+a = 5
+
+print(a)
+
+print(a+a)
+
+a = a * 3
+
+print(a)
+
+receita = 1000
+margem_lucro = 0.2
+lucro = margem_lucro * receita
+print(lucro)
---
+++ Curso_udemy/strings.py
@@ -0,0 +1,36 @@
+print('Olá')
+
+#string
+print(type('Olá'))
+
+#aspas duplas para usar aspas nas frases
+print("There's")
+print('A quanto "tempo" pessoal')
+
+#len conta cataracteres
+print(len("tamanho do texto"))
+
+a = 'A quanto "tempo" pessoal'
+
+#tras a posição na string
+print(a[3])
+
+#trás a string da posição x a y
+print(a[2:8])
+print(a[2:])
+print(a[-7:])
+print(a[:-7])
+print(a[-7])
+
+#concatenar string
+print(a + ' teste')
+
+#repetir a letra 10 vezes
+letra = 'r'
+print(letra * 10)
+
+#toda string em minusculo
+print(a.lower())
+
+#transforma astring em lista
+print(a.split())
